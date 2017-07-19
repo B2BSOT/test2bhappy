94dfcdb2f6899c86527c77eaaba927b61111142d
dadembii <dadembii@gmail.com>
2017-07-19 04:55:06 +0000

    update vote model

--- models/vote_detail.js
+++ models/vote_detail.js
@@ -5,7 +5,6 @@ module.exports = function(sequelize, DataTypes) {
                         vote_id: {type: DataTypes.INTEGER, primaryKey: true},
                         item_id: {type: DataTypes.INTEGER, primaryKey: true},
                         user_id: {type: DataTypes.INTEGER, primaryKey: true},
-                        reg_user_id: {type: DataTypes.INTEGER, allowNull: false},
                         reg_dtm: {type: DataTypes.STRING, allowNull: false},
                     },{
                         classMethods: {},
--- models/vote_master.js
+++ models/vote_master.js
@@ -17,10 +17,10 @@ module.exports = function(sequelize, DataTypes) {
                         noti_yn: {type: DataTypes.STRING, allowNull: false}
                     },{
                         classMethods: {},
-                        tableName: 'vote_master',
-                        freezeTableName: true,
+                        tableName: 'vote_master', // 쿼리에서의 테이블 이름을 설정
+                        freezeTableName: true,  // true이면 tableName에 설정한 이름으로만 쿼리 생성
                         underscored: true,
-                        timestamps: false
+                        timestamps: false // true이면 자동으로 등록시간, 수정시간 만들어줌
                     });
                     
     return vote_master;
