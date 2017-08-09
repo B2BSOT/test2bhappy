0885e90c57589e7d6f3ad1961acbb1d39e75e868
JuneBeomKim <myoro1d@naver.com>
2017-08-09 00:08:09 +0000

    a

--- models/com_org.js
+++ models/com_org.js
@@ -2,7 +2,6 @@
 
 module.exports = function(sequelize, DataTypes) {
     var com_org = sequelize.define('com_org', {
-<<<<<<< HEAD
                         org_id:       {type: DataTypes.INTEGER, primaryKey: true},
                         org_nm:       {type: DataTypes.STRING, allowNull: true},
                         org_level:    {type: DataTypes.INTEGER, allowNull: false},
@@ -17,19 +16,3 @@ module.exports = function(sequelize, DataTypes) {
                     
     return com_org;
 }
-=======
-                        org_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-                        org_nm: {type: DataTypes.STRING, allowNull: false},
-                        org_level: {type: DataTypes.INTEGER, allowNull: false},
-                        upper_org_id: {type: DataTypes.INTEGER, allowNull: false}
-                    },{
-                        classMethods: {},
-                        tableName: 'com_org', // 쿼리에서의 테이블 이름을 설정
-                        freezeTableName: true, // true이면 tableName에 설정한 이름으로만 쿼리 생성
-                        underscored: true,
-                        timestamps: false // true이면 자동으로 등록시간, 수정시간 만들어줌
-                    });
-                    
-    return com_org;
-}
->>>>>>> b3b159e9905dc2c09622b9ac59ce1e2ab36bad57
